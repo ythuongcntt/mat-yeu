dde2fbce71b1c3208445b66e084fab31a3d17e12
ythuongcntt <101122525+ythuongcntt@users.noreply.github.com>
2022-03-07 22:30:29 +0700

    Add files via upload
    matiuanh

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Quà nào';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Hãy mở hộp';
+const titleCard = 'Tặng Mật iu';
+const contentCard = 'Chúc Mật yêu của anh ngày 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Yêu em!';
+
+
+const giftImage = 'em.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
